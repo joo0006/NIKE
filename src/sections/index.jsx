bcca432fafc4cdab44bdaca53aac1c326824a0bf
joo0006 <jyotisingh0966@gmail.com>
2024-01-17 19:49:44 +0530

    first commit

---
+++ src/sections/index.jsx
@@ -0,0 +1,20 @@
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
+import Services from "./sections/Services";
+import SpecialOffers from "./sections/SpecialOffers";
+import CustomerReviews from "./sections/CustomerReviews";
+import Subscribe from "./sections/Subscribe";
+import Footer from "./sections/Footer";
+
+
+export {
+CustomerReviews,
+Footer,
+Hero,
+PopularProducts,
+Services,
+SpecialOffers,
+Subscribe,
+SuperQuality,
+};
